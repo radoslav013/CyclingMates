aaa32a9d416c9ab5d1497607d6bce50636510f6d
radoslavradev <radoslav0134@gmail.com>
2023-01-16 13:50:36 +0200

    Make the register page allowed for unathenticated

--- CyclingMates/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CyclingMates/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -22,6 +22,7 @@ using Microsoft.Extensions.Logging;
 
 namespace CyclingMates.Areas.Identity.Pages.Account
 {
+    [AllowAnonymous]
     public class RegisterModel : PageModel
     {
         private readonly SignInManager<CyclingMatesUser> _signInManager;
